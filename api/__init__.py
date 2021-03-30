91e7d6ae8ddc06e1467c9a198b1140844b87c65d
Lucas-AV7 <lucas.av7@yahoo.com>
2021-03-29 21:27:20 -0300

    Custom 404 and 405 http response

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, abort, jsonify
 from .blueprints.users_blueprint import users_bp
 from .blueprints.users_address_blueprint import users_address_bp
 from .models import configure_db
@@ -22,3 +22,22 @@ configure_db(app)
 
 app.register_blueprint(users_bp, url_prefix='/api')
 app.register_blueprint(users_address_bp, url_prefix='/api')
+
+
+# Custom error response for all api endpoints
+@app.errorhandler(405)
+def not_allowed(e):
+    return jsonify({
+        "status": "Error",
+        "status_code": 405,
+        "message": "Method not allowed"
+    }), 405
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({
+        "status": "Error",
+        "status_code": 404,
+        "message": "API endpoint not found"
+    }), 404
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,27 @@
-from api import app
+import api
 from flask import Flask
+import json
 
 
 def test_if_api_has_flask_app():
-    assert isinstance(app, Flask)
+    assert isinstance(api.app, Flask)
+
+
+def test_if_api_has_405_error_handler():
+    assert hasattr(api, "not_allowed")
+    expected_error = {
+        "status": "Error",
+        "status_code": 405,
+        "message": "Method not allowed"
+    }
+    assert expected_error == json.loads(api.not_allowed("")[0].response[0])
+
+
+def test_if_api_has_404_error_handler():
+    assert hasattr(api, "not_found")
+    expected_error = {
+        "status": "Error",
+        "status_code": 404,
+        "message": "API endpoint not found"
+    }
+    assert expected_error == json.loads(api.not_found("")[0].response[0])
